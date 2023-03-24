92f1da2d7d0d7e3f1fbe12d847a398670810c5e9
CampbellZach <zachary.campbell23@outlook.com>
2023-03-24 14:43:42 -0600

    90% implementation of the login and admin page you can not type it in to enter you must login used sessions, last thing needed is password hashing

--- prototype/login.php
+++ prototype/login.php
@@ -1,48 +1,94 @@
 <?php 
+session_start();
+/*$dns = 'mysql:host=localhost;dbname=cs_350';
+$username = 'student';
+$p = 'CS350';
+$db = new PDO($dns, $username, $p);
+*/
 $servername = 'localhost';
 $username = 'student';
 $password = 'CS350';
 $dbname = 'tour_db';
+$_SESSION['isVerified'] = -1;
 
 
-$conn = new mysqli($servername,$username,$password,$dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error){
-  die("Connection failed: ".$conn->connect_error);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-
 if (isset($_POST['submit'])) {
+  $username = $_POST['username'];
+  $passwd = $_POST['password'];
+
+  $sql = "SELECT * FROM users WHERE uName = '$username' AND passwd = '$passwd'";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    $users = array();
+    while($row = $result->fetch_assoc()){
+        $user = array(
+          'id' => $row['id'],
+          'name' => $row['uName'],
+        );
+        array_push($users,$user);
+      }
+    // Username and password match
+    $id = $sites['id'];
+    session_regenerate_id();
+    $_SESSION['isVerified'] = 1;
+    $_SESSION['name'] = $username;
+    $_SESSION['id'] = $id;
+    echo "<script>location.href='admin.php';</script>";
+  } else {
+    // Username and password do not match
+    echo "Invalid username or password.";
+  }
+}
+
+/*
+if($_SERVER["REQUEST_METHOD"] == "POST"){    
     $username = $_POST['username'];
     $password = $_POST['password'];
   
-    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-    $result = $conn->query($sql);
-  
-    if ($result->num_rows > 0) {
-      // Username and password match
-      echo "Login successful!";
+    $sql = "SELECT * FROM users WHERE uName = :username AND passwd = :password";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+    
+    if ($stmt->rowCount() > 0) {
+        // Username and password match
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $id = $row['id'];
+        $passwd = $row['passwd'];
+        if($password === $passwd){
+            session_regenerate_id();
+            $_SESSION['isVerified'] = 1;
+            $_SESSION['name'] = $username;
+            $_SESSION['id'] = $id;
+            echo "<script>location.href='admin.php';</script>";
+        }
     } else {
-      // Username and password do not match
-      echo "Invalid username or password.";
+        // Username and password do not match
+        echo "Invalid username or password.";
     }
-  }
-
+}*/
 ?>
 
-
 <html>
-    <head> 
+<head> 
 
-    </head>
-    <body>
+</head>
+<body>
+    <h1>Login</h1>
+
     <form method="POST">
-        <label for="username">UserName:</label>
-        <label for="n">UserName:</label>
-        <input type="text" name="username" required>
-        <input type="password" name="password" required>
+        <label for="username">Username:</label>
+        <input type="text" name="username" required><br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" required><br>
         <input type="submit" name="submit" value="Login">
     </form>
-    </body>
+</body>
 </html>
-
